315a4a65b49a7f29ca8606aa3f6c0bfcd1035873
belyakova-anna <belyakova.anna.st@yandex.ru>
2024-07-02 10:48:36 +0300

    path change

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/Front-end-Web-Development-IU/",
+  base: "/Front-end-Web-Development-IU/",
   plugins: [react()],
   build: {
     target: 'esnext',
